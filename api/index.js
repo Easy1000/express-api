6b5096bc1048f5ce2d4586f041998a04fe75ae86
easy <izaaz240@gmail.com>
2023-04-10 21:02:13 +0700

    feat(server): add redirect from root to api endpoint

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.redirect('/api')
+})
+
 app.get("/api", (req, res) => {
   res.send("Express on Vercel");
 });
