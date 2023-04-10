10c41fe997599653a50b9153e7d6434b0f0384cc
easy <izaaz240@gmail.com>
2023-04-10 21:09:41 +0700

    refactor(server): delete unnecessary code

--- api/index.js
+++ api/index.js
@@ -2,10 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get('', (req, res) => {
-  res.redirect('/api')
-})
-
 app.get("/api", (req, res) => {
   res.send("Express on Vercel");
 });
