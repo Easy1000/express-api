3575cf056d40d111876088992515e8777f192502
easy <izaaz240@gmail.com>
2023-05-06 21:12:02 +0700

    feat(server): add static file

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.sendFile('index.html')
 });
 
 app.listen(3000)
 
-// export default app
+export default app
